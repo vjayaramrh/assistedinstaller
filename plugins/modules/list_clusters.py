05c1549a74c6cf16aa7061b852264f38e0f2ccf1
Vishwanath Jayaraman <vjayaram@redhat.com>
2024-11-01 16:08:44 -0500

    add support for the API query parameter 'with_hosts' (#10)

--- plugins/modules/list_clusters.py
+++ plugins/modules/list_clusters.py
@@ -20,6 +20,10 @@ version_added: "1.0.0"
 description: Given the API token, returns list of clusters in the account
 
 options:
+    with_hosts:
+        description: Include hosts in the returned list
+        default: false
+        type: bool
     fail:
         description: Trigger a failure
         default: false
@@ -47,15 +51,25 @@ list_clusters_msg:
 from ansible.module_utils.basic import AnsibleModule
 
 BASE_URL = "https://api.openshift.com/api/assisted-install/v2/clusters"
+# add additional query parameters to the query_params_list
+QUERY_PARAMS_LIST = ["with_hosts"]
 
 def run_module():
     module_args = dict(
         fail=dict(type="bool", required=False, default=False),
+        # any API query parameters may have to be added here
+        with_hosts=dict(type="bool", required=False, default=False),
     )
     token = os.environ.get('AI_API_TOKEN')
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
 
-    response = requests.get( BASE_URL, headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'} )
+    q_params = {}
+    for k in QUERY_PARAMS_LIST:
+        val = module.params.get(k)
+        if val:
+            q_params = q_params | { k: val }
+    # reference https://docs.python-requests.org/en/latest/user/quickstart/
+    response = requests.get( BASE_URL, params=q_params, headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'} )
 
     if not response.ok:
         result = dict(changed=True, list_clusters_msg=response.text)
