0b71412ac292042b504ae5b5bb3651616924ccb8
Chris Wheeler <chwheele@redhat.com>
2024-11-11 14:20:49 -0800

    fixed errors from sanity test

--- plugins/modules/features.py
+++ plugins/modules/features.py
@@ -3,10 +3,6 @@
 
 from __future__ import absolute_import, division, print_function
 
-import os
-import requests
-
-
 __metaclass__ = type
 
 DOCUMENTATION = r"""
@@ -23,32 +19,31 @@ options:
   openshift_version:
     description: Version of OpenShift
     required: True
-    type: string
+    type: str
 
   cpu_architecture:
     description: The CPU architecture of the OpenShift version
     required: False
     choices: [x86_64, aarch64, arm64, ppc64le, s390x, multi]
     default: x86_64
-    type: string
+    type: str
 
   platform_type:
     description: The provider platform type
     required: False
     choices: [baremetal, none, nutanix, vsphere, external]
-    type: string
+    type: str
 
-  external_platform:
+  external_platform_name:
     description: External platform name when platform_type is external
     required: False
-    type: string
+    type: str
 
 author:
     - Chris Wheeler (@clwheel)
 """
 
 EXAMPLES = r"""
-# Use argument
 - name: Query OpenShift features
   features:
     openshift_version: 4.16.19
@@ -84,21 +79,38 @@ features:
 """
 
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import missing_required_lib
+
+import os
+import traceback
+
+try:
+    import requests
+except ImportError:
+    HAS_REQUESTS = False
+    REQUESTS_IMPORT_ERROR = traceback.format_exc()
+else:
+    HAS_REQUESTS = True
+    REQUESTS_IMPORT_ERROR = None
 
 API_VERSION = "v2"
 API_URL = f"https://api.openshift.com/api/assisted-install/{API_VERSION}"
 QUERY_PARAMS_LIST = ["openshift_version", "cpu_architecture", "platform_type", "external_platform_name"]
 
+
 def run_module():
     module_args = dict(
-        openshift_version = dict(type="str", required=True),
-        cpu_architecture = dict(type="str", required=False, default="x86_64", choices=["x86_64", "aarch64", "arm64", "ppc64le", "s390x", "multi"]),
-        platform_type = dict(type="str", required=False, choices=["baremetal", "none", "nutanix", "vsphere", "external"]),
-        external_platform_name = dict(type="str", required=False)
+        openshift_version=dict(type="str", required=True),
+        cpu_architecture=dict(type="str", required=False, default="x86_64", choices=["x86_64", "aarch64", "arm64", "ppc64le", "s390x", "multi"]),
+        platform_type=dict(type="str", required=False, choices=["baremetal", "none", "nutanix", "vsphere", "external"]),
+        external_platform_name=dict(type="str", required=False)
     )
 
     token = os.environ.get('AI_API_TOKEN')
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
+
+    # Fail if requests is not installed
+    module.fail_json(msg=missing_required_lib('requests'), exception=REQUESTS_IMPORT_ERROR)
 
     # Set headers
     headers = {
@@ -111,7 +123,7 @@ def run_module():
     for k in QUERY_PARAMS_LIST:
         val = module.params.get(k)
         if val:
-            query_params = query_params | { k: val }
+            query_params = query_params | {k: val}
 
     response = requests.get(f"{API_URL}/support-levels/features", params=query_params, headers=headers)
 
@@ -120,7 +132,7 @@ def run_module():
         module.fail_json(msg="Error querying features", **result)
 
     result = response.json()
-    
+
     module.exit_json(**result)
 
 
