de944ee575784d8e6e1678814e1b58400ab7e79c
Daniel Kostecki <dkosteck@redhat.com>
2025-02-27 16:23:30 -0500

    formatting and adding absent required_if

--- plugins/modules/clusters.py
+++ plugins/modules/clusters.py
@@ -90,45 +90,58 @@ QUERY_PARAMS_LIST = ["with_hosts"]
 def run_module():
 
     module_args = dict(
-        state=dict(type="str", required=False, choices=["absent", "present"], default=None),
+        state=dict(
+            type="str", required=False, choices=["absent", "present"], default=None
+        ),
         cluster_id=dict(type="str", required=False),
         # any API query parameters may have to be added here
         with_hosts=dict(type="bool", required=False, default=False),
         name=dict(type="str", required=False),
-        openshift_version=dict(type="str", required=False)
+        openshift_version=dict(type="str", required=False),
     )
-    token = os.environ.get('AI_API_TOKEN')
-    module = AnsibleModule(argument_spec=module_args, required_if=[('state', 'present', ['name', 'openshift_version'])], supports_check_mode=False)
+    token = os.environ.get("AI_API_TOKEN")
+    module = AnsibleModule(
+        argument_spec=module_args,
+        required_if=[
+            ("state", "present", ["name", "openshift_version"]),
+            ("state", "absent", ["cluster_id"]),
+        ],
+        supports_check_mode=False,
+    )
 
     # Fail if requests is not installed
     if not HAS_REQUESTS:
-        module.fail_json(msg=missing_required_lib('requests'), exception=REQUESTS_IMPORT_ERROR)
+        module.fail_json(
+            msg=missing_required_lib("requests"), exception=REQUESTS_IMPORT_ERROR
+        )
 
     # Set headers
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {token}'
-    }
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
 
     # Delete cluster
-    if module.params.get('state') == "absent":
-        if not module.params.get('cluster_id'):
+    if module.params.get("state") == "absent":
+        if not module.params.get("cluster_id"):
             module.fail_json(msg="cluster_id is required for delete action")
 
-        response = requests.delete(f"{API_URL}/clusters/{module.params.get('cluster_id')}", headers=headers)
+        response = requests.delete(
+            f"{API_URL}/clusters/{module.params.get('cluster_id')}", headers=headers
+        )
 
         if response.status_code != 204:
             result = dict(response=response.text)
-            module.fail_json(msg=f"Error deleting cluster_id: {module.params.get('cluster_id')}", **result)
+            module.fail_json(
+                msg=f"Error deleting cluster_id: {module.params.get('cluster_id')}",
+                **result,
+            )
 
         result = dict(changed=True, clusters=[])
 
     # Register cluster
-    elif module.params.get('state') == "present":
-        pull_secret = os.environ.get('AI_PULL_SECRET')
+    elif module.params.get("state") == "present":
+        pull_secret = os.environ.get("AI_PULL_SECRET")
 
         data = remove_module_fields(module)
-        data['pull_secret'] = pull_secret
+        data["pull_secret"] = pull_secret
 
         response = requests.post(f"{API_URL}/clusters", headers=headers, json=data)
 
@@ -146,7 +159,9 @@ def run_module():
             if val:
                 list_params = list_params | {k: val}
 
-        response = requests.get(f"{API_URL}/clusters", params=list_params, headers=headers)
+        response = requests.get(
+            f"{API_URL}/clusters", params=list_params, headers=headers
+        )
 
         if not response.ok:
             result = dict(changed=True, response=response.text)
