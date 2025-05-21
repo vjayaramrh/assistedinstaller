c30f12827568e2c47d16c8d20af4a0eaaef4d31a
Chris Wheeler <chwheele@redhat.com>
2025-05-21 11:38:26 -0700

    updated events module to use apitoken.GetToken()

--- plugins/modules/events.py
+++ plugins/modules/events.py
@@ -94,6 +94,11 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.basic import missing_required_lib
 
 try:
+    from ansible_collections.openshift_lab.assisted_installer.plugins.module_utils import apitoken
+except ImportError:
+    from ansible.module_utils import apitoken
+
+try:
     import requests
 except ImportError:
     HAS_REQUESTS = False
@@ -119,7 +124,7 @@ def run_module():
         severities=dict(type="list", elements="str", required=False, choices=["info", "warning", "error", "critical"]),
     )
 
-    token = os.environ.get('AI_API_TOKEN')
+    token = apitoken.GetToken()
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
 
     # Fail if requests is not installed
